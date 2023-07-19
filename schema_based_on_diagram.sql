75b370f5a87b6655e583a2793f22e25a97a1b648
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-19 17:22:07 +0330

    Create indexes on foreign key columns.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -73,3 +73,14 @@ CREATE TABLE medical_history_treatments (
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
   FOREIGN KEY (treatment_id) REFERENCES treatments (id)
 );
+
+-- Create indexes on foreign key columns
+CREATE INDEX idx_medical_history_treatments_medical_history_id ON medical_history_treatments (medical_history_id);
+CREATE INDEX idx_medical_history_treatments_treatment_id ON medical_history_treatments (treatment_id);
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
